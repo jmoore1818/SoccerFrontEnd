f4c616c7ad8cb7cba7adae11e4c3c8ea95513fad
jmoore1818 <Justindonaldmoore@gmail.com>
2016-09-09 15:45:21 -0400

    SoccerFrontEnd

---
+++ api/controllers/PlayersController.js
@@ -0,0 +1,113 @@
+/**
+ * EmployeeController
+ *
+ * @description :: Server-side logic for managing employees
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+var Client = require('node-rest-client').Client;
+var client = new Client();
+var endpoint = "http://localhost:1337/players"
+
+module.exports = {
+
+  /**
+   * `PlayersController.create()`
+   */
+  create: function (req, res) {
+        
+        if(req.method != "POST"){
+          return res.view('create');
+        }
+
+        var args = {
+            data: req.body,
+            headers: { "Content-Type": "application/json" }
+        };
+         
+        client.post(endpoint, args, function (data, response) {
+            // return res.view('create', {success: { message: "Record added successfully"}});
+            if(response.statusCode != "201"){
+                return res.view('create', {error:{message: response.statusMessage + ": " + data.reason}});
+            }
+
+            return res.view('create', {success:{message: "Record created successfully"}});
+
+        })
+ 
+  },
+
+
+  /**
+   * `PlayersController.read()`
+   */
+  read: function (req, res) {
+
+    client.get(endpoint, function (data, response) {
+        return res.view('read', {players: data});
+    }).on('error', function (err) {
+        return res.view('read', {error: { message: "There was an error getting the records"}});
+    });
+
+  },
+
+
+  /**
+   * `PlayersController.update()`
+   */
+  update: function (req, res) {
+       
+
+  if(req.method != "POST"){
+    return res.view('update');
+    } 
+
+    var args = {
+        data: req.body,
+        headers: { "Content-Type": "application/json" }
+    };  
+  
+  var players = req.allParams();
+  //creates new URL for PUT method
+  var endpointUp = endpoint + "/" +  players.id;
+  
+  client.put(endpointUp, args, function (data, response) {
+   if(response.statusCode != "200"){
+                return res.view('update', {error:{message: response.statusMessage + ": " + data.reason}});
+            }
+
+            return res.view('update', {success:{message: "Record created successfully"}});
+
+        })
+  },
+
+
+  /**
+   * `PlayersController.delete()`
+   */
+  delete: function (req, res) {
+    
+ if(req.method != "POST"){
+    return res.view('delete');
+    } 
+
+    var args = {
+        data: req.body,
+        headers: { "Content-Type": "application/json" }
+    };  
+  
+  var players = req.allParams();
+  //creates new URL for PUT method
+  var endpointUp = endpoint + "/" +  players.id;
+  
+  client.delete(endpointUp, args, function (data, response) {
+   if(response.statusCode != "200"){
+                return res.view('delete', {error:{message: response.statusMessage + ": " + data.reason}});
+            }
+
+            return res.view('delete', {success:{message: "Record deleted successfully"}});
+
+  })
+}
+};
+
